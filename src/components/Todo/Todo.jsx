6f15f33c469ee2a06e351b25c8b78da815f46f8a
yasinosman <yasinosman10@gmail.com>
2022-04-14 00:08:12 +0300

    refactor(Todo): add better comments

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+// eslint-disable-next-line no-unused-vars
 import TodoModel from "./TodoModel";
 
 /**
  * @param {Object} props
  * @param {TodoModel} props.todo - TodoModel instance
- * @param {Function} props.onClick - Callback function to be called when the todo is clicked
+ * @param {(todo: TodoModel) => void} props.onClick - Callback function to be called when the todo is clicked
  */
 const Todo = ({ todo, onClick }) => {
 	const handleClick = React.useCallback(() => {
